d30cf2d0a6ecb9eeda667c0cf7407b8f6f6dee36
Thanawat Gulati <work.t6tg@gmail.com>
2021-03-30 17:43:34 +0700

    feat: connect firebase

--- dashboard/script/main.js
+++ dashboard/script/main.js
@@ -1,3 +1,13 @@
+var app = firebase.initializeApp({
+    apiKey: 'AIzaSyApRU3e_kLBwl1Rib4B9DIVdHjjcXX48iA',
+    authDomain: 'iot-kmutnb-2020.firebaseapp.com',
+    projectId: 'iot-kmutnb-2020',
+    storageBucket: 'iot-kmutnb-2020.appspot.com',
+    messagingSenderId: '945437931517',
+    appId: '1:945437931517:web:7272957d6f364df69e8b94',
+    measurementId: 'G-MC011V3DL9',
+})
+
 const db = firebase.firestore()
 db.collection('temp')
     .get()
